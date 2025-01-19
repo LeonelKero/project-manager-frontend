aca13aa8157faa5fdc36549181295a8ebb255dd6
leonel ka <waboleonel@gmail.com>
2025-01-19 15:43:30 +0100

    Code refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
-import ProductTable from "./components/ProductTable.tsx";
-import {Container, CssBaseline} from "@mui/material";
+import { Container, CssBaseline } from "@mui/material";
+// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AAppBar from "./components/AAppBar.tsx";
+import AppButton from "./components/AppButton.tsx";
+import ProductTable from "./components/ProductTable.tsx";
+import useCategoryQuery from "./hooks/useCategoryQuery.ts";
+
+// const queryClient = new QueryClient();
 
 function App() {
+  
+  const { data } = useCategoryQuery();
+  console.log(data);
 
-    return (
-        <Container maxWidth={"xl"}>
-            <CssBaseline/>
-            <AAppBar />
-            <ProductTable/>
-        </Container>
-    )
+  return (
+    <Container maxWidth={"xl"}>
+      <CssBaseline />
+      <AAppBar />
+      {/* <QueryClientProvider client={queryClient}> */}
+      <AppButton label={"Add"} />
+      <ProductTable />
+      {/* </QueryClientProvider> */}
+    </Container>
+  );
 }
 
-export default App
+export default App;
