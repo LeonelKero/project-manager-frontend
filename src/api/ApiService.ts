b92dd52315d509779f1b1718679aaa76560190a5
leonel ka <waboleonel@gmail.com>
2025-01-27 22:11:26 +0100

    Implement generic methods for backend API calls

--- src/api/ApiService.ts
+++ src/api/ApiService.ts
@@ -1,6 +1,10 @@
-import { AxiosResponse } from "axios";
+import {AxiosResponse} from "axios";
 import apiClient from "./apiClient";
 
+/**
+ * This class holds methods that any service can refine accordingly to specific needs
+ * on backend API call.
+ */
 class AbstractApiService<T> {
     endpoint: string
 
@@ -8,12 +12,51 @@ class AbstractApiService<T> {
         this.endpoint = endpoint
     }
 
-    async get(uri?: string, params?: any): Promise<AxiosResponse<T[], any>> {
+    /**
+     * Generic method for fetching any entity of type 'T'.
+     * @param uri of the targeted resource.
+     * @param params extra parameters for axios configuration.
+     */
+    async get(uri?: string, params?: unknown): Promise<AxiosResponse<T[], unknown>> {
         return apiClient(params).get<T[]>(this.endpoint + uri)
     }
 
-    async post<S>(uri: string, data: S, params?: any): Promise<AxiosResponse<T, any>> {
-        return apiClient(params).post<T>(this.endpoint + uri, data, { headers: { "Content-Type": "application/json" } })
+    /**
+     * Generic method for creating new entity of 'S' data type.
+     * @param uri of the targeted resource.
+     * @param data to be persisted.
+     * @param params extra parameters for axios configuration.
+     */
+    async post<S>(uri: string, data: S, params?: unknown): Promise<AxiosResponse<T, unknown>> {
+        return apiClient(params).post<T>(this.endpoint + uri, data, {headers: {"Content-Type": "application/json"}})
+    }
+
+    /**
+     * Generic method for deleting a resource.
+     * @param uri of the targeted resource
+     * @param params extra parameters for axios configuration
+     */
+    async delete(uri: string, params?: unknown): Promise<AxiosResponse<T, unknown>> {
+        return apiClient(params).delete<T>(this.endpoint + uri)
+    }
+
+    /**
+     * Generic method for getting a single element of type 'T'.
+     * @param uri of the targeted resource.
+     * @param params extra parameters for axios configuration.
+     */
+    async getOne(uri: string, params?: unknown): Promise<AxiosResponse<T, unknown>> {
+        return apiClient(params).get<T>(this.endpoint + uri)
+    }
+
+    /**
+     * Generic method for updating resource of 'T' type. Request is made with data of type 'S'
+     * @param uri of the targeted resource
+     * @param data request data as update
+     * @param params extra parameters for axios configuration.
+     */
+    async update<S>(uri: string, data: S, params?: unknown): Promise<AxiosResponse<T, unknown>> {
+        return apiClient(params).put<T>(uri, data);
     }
 }
 
